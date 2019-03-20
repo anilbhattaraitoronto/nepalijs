173d89272c9886ab7da862be37e50e43a8860065
Anil Bhattarai <anil.bhattarai@mail.utoronto.ca>
2019-03-20 18:44:22 -0400

    First Commit

---
+++ main.js
@@ -0,0 +1,55 @@
+
+var index = 1;
+var nextButton = document.querySelector('.next-button');
+var prevButton = document.querySelector('.prev-button')
+var lessons = document.querySelectorAll('.lesson');
+var refreshButton = document.querySelector('.refresh-button');
+
+//This can be used for slideshows for other sites
+setInitial();
+function setInitial() {
+
+    index++;
+    if (index > lessons.length) {
+        index = 1
+    }
+    if (index < 1) {
+        index = lessons.length
+    }
+    lessons[index - 1].style.display = 'block';
+
+}
+
+function hideLessons() {
+    lessons.forEach((lesson) => {
+        lesson.style.display = 'none'
+    })
+}
+function addLesson() {
+    hideLessons()
+    setInitial()
+    console.log(index)
+}
+
+function showLessons() {
+    lessons.forEach((lesson) => {
+        lesson.style.display = 'block'
+    })
+    index = 1;
+}
+
+prevButton.addEventListener('click', () => {
+    index--;
+    if (index < 1) {
+        index = lessons.length
+    }
+    hideLessons()
+    lessons[index - 1].style.display = 'block';
+
+})
+nextButton.addEventListener('click', addLesson)
+refreshButton.addEventListener('click', showLessons)
+
+
+
+
