f3c51a57801e94e5d58c62071e7138945b43127f
Anil Bhattarai <anil.bhattarai@mail.utoronto.ca>
2019-03-21 12:43:30 -0400

    four sections navigation added with javascript function. filled those sections with lorem ipsum for now.

--- main.js
+++ main.js
@@ -4,11 +4,21 @@ var index = 1;
 var nextButton = document.querySelector('.next-button');
 var prevButton = document.querySelector('.prev-button')
 var lessons = document.querySelectorAll('.lesson');
-var refreshButton = document.querySelector('.refresh-button');
+
+var mainIndex = 1;
+var mainSections = document.querySelectorAll('.main-section')
+var lessonTab = document.querySelector('.lesson-tab')
+var readTab = document.querySelector('.read-tab')
+var writeTab = document.querySelector('.write-tab')
+var listenTab = document.querySelector('.listen-tab')
+
+// var lessonTab = document.querySelector('.lesson-tab');
 
 //This can be used for slideshows for other sites
-setInitial();
-function setInitial() {
+// setInitialMain();
+setInitialLesson();
+showSection(1);
+function setInitialLesson() {
 
     if (index > lessons.length) {
         index = 1
@@ -19,31 +29,72 @@ function setInitial() {
     lessons[index - 1].style.display = 'block';
 }
 
+
 function hideLessons() {
     lessons.forEach((lesson) => {
         lesson.style.display = 'none'
     })
 }
-function addLesson() {
+function nextLesson() {
     hideLessons()
     index++;
-    setInitial()
+    setInitialLesson()
+
 }
 
-function showLessons() {
-    lessons.forEach((lesson) => {
-        lesson.style.display = 'block'
-    })
-    index = 1;
-}
-
-prevButton.addEventListener('click', () => {
+function prevLesson() {
     hideLessons()
     index--;
-    setInitial()
+    setInitialLesson()
+}
+function lessonRefresh() {
+    index = 1
+    hideLessons()
+    lessons[index - 1].style.display = 'block';
+}
+
+
+prevButton.addEventListener('click', prevLesson)
+nextButton.addEventListener('click', nextLesson)
+// lessonTab.addEventListener('click', refresh);
+
+
+//Main Sections Code
+
+// showInitialSection();
+
+// function showInitialSection() {
+
+// }
+function showSection(n) {
+    // showInitialSection()
+    if (n > mainSections.length) {
+        mainIndex = 1
+    }
+    if (n < 1) {
+        mainIndex = mainSections.length
+    }
+    mainSections.forEach((section) => {
+        section.style.display = 'none'
+    })
+    mainSections[n - mainIndex].style.display = 'block'
+
+}
+lessonTab.addEventListener('click', () => {
+    showSection(1)
+    lessonRefresh();
 })
-nextButton.addEventListener('click', addLesson)
-refreshButton.addEventListener('click', showLessons)
+readTab.addEventListener('click', () => {
+    // refresh();
+    showSection(2)
+})
+writeTab.addEventListener('click', () => {
+    showSection(3)
+})
+listenTab.addEventListener('click', () => {
+    showSection(4)
+})
+
 
 
 
